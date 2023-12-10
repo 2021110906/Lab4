45865fcc6d048d0e745b854ed295ed69234ade7a
怀瑾 <13084101+zhou___xiang@user.noreply.gitee.com>
2023-12-10 20:54:58 +0800

    work2

--- TEST/SolutionTest.java
+++ TEST/SolutionTest.java
@@ -15,7 +15,7 @@ public class SolutionTest {
     public void testExample1() {
         int[] nums = {-1, 0, 1, 2, -1, -4};
         List<List<Integer>> expected = Arrays.asList(
-                Arrays.asList(-1, -1, 2),
+                Arrays.asList(-1, -4, 2),
                 Arrays.asList(-1, 0, 1)
         );
         List<List<Integer>> result = solution.threeSum(nums);
